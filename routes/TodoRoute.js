be17159ffbde71b41090c963e6c25ff7443b3d92
Arun Kumar R <125977488+aruntutter@users.noreply.github.com>
2024-06-01 17:34:07 +0530

    Initial commit: Add backend files

---
+++ routes/TodoRoute.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const {
+  saveTodo,
+  getTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/TodoController");
+
+const router = Router();
+
+router.get("/", getTodo);
+router.post("/save", saveTodo);
+router.post("/update", updateTodo);
+router.post("/delete", deleteTodo);
+
+module.exports = router;
